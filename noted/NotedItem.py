2cc763ed5b3ae3675b1256fa6a78955892afd2b2
Zachary Ashen <zachary.h.a@gmail.com>
2020-06-13 13:30:37 -0400

    add functions for list manipulation

--- noted/NotedItem.py
+++ noted/NotedItem.py
@@ -6,6 +6,7 @@ Date: June 4th 2020
 
 import re
 
+
 class _NotedItem(object):
     """
     """
@@ -22,14 +23,13 @@ class NoteItem(_NotedItem):
 
     def toDict(self):
         listItemDict = {
-            'title' : self.title,
-            'body' : self.bodyText
+            'title': self.title,
+            'body': self.bodyText
         }
         return listItemDict
 
+
 class ListItem(_NotedItem):
-    """
-    """
     def __init__(self, title, *items):
         super().__init__(title)
         self.items = items
@@ -44,20 +44,38 @@ class ListItem(_NotedItem):
     def getUncheckedItems(self):
         uncheckedList = []
         for item in self.items[0]:
-            uncheckedList.append('□ ' + item[0])
+            if not item[1]:
+                uncheckedList.append('□ ' + item[0])
         return uncheckedList
 
-    def addItem(self, item):
+    def add_item(self, item):
         pass
 
-    def deleteItem(self, item):
-        pass
+    def delete_item(self, item_name):
+        for item in self.items[0]:
+            if item_name == item[0]:
+                print(self.items[0] )
+                index_of_item = self.items[0].index(item)
+                self.items[0].pop(index_of_item)
 
-    def checkItem(self, item):
-        pass
+    def rename_item(self, old_name, new_name):
+        for item in self.items[0]:
+            if old_name == item[0]:
+                item_list = list(item)
+                item_list[0] = new_name
+                index_of_item = self.items[0].index(item)
+                self.items[0][index_of_item] = tuple(item_list)
 
-    def setItem(self, item):
-        pass
+
+    def check_item(self, items_to_check):
+        for item in self.items[0]:
+            for item_to_check in items_to_check:
+                if item[0] == item_to_check:
+                    index_of_item = self.items[0].index(item)
+                    item_list = list(item)
+                    item_list[1] = True
+                    item = tuple(item_list)
+                    self.items[0][index_of_item] = item
 
     def toDict(self):
         listItemDict = {
